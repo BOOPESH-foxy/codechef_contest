86639e1b8b9c9300fc4bf849ccb905e17df0426d
BOOPESH-foxy <boopesh.mc@gmail.com>
2022-09-21 20:10:26 +0530

    eyrc_codechef dec_bin2

--- src/scripts/dec_bin.py
+++ src/scripts/dec_bin.py
@@ -1,9 +1,8 @@
 def dec_to_binary(n):
-    print('{:08b}'.format(n))
-    # if n>1:
-    #     dec_to_binary(n//2)
-    # print(n%2,end ="")
-    # print(bin(format(n,'08b')))
+    if n>1:
+        dec_to_binary(n//2)
+    print(n%2,end = "")
+
 
 
 t = int(input())
@@ -12,4 +11,4 @@ if(t>=1 and t<=25):
         dec_data = int(input())
         if(dec_data>=1 and dec_data<=255):
             dec_to_binary(dec_data)
-            print()
+        print()

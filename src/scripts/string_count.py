fe5aae71fe0310f371cb407faf39220ce91a4a9e
BOOPESH-foxy <boopesh.mc@gmail.com>
2022-09-21 17:57:13 +0530

    eyrc_codechef string count

--- src/scripts/string_count.py
+++ src/scripts/string_count.py
@@ -1,12 +1,21 @@
+from curses.ascii import isalpha
+from turtle import st
+
+
 t = int(input())
 for _ in range(t):
     str_1 = str(input())
     i = 0
     count = 0
     for i in range(len(str_1)):
-        if(str_1[i] == ' '):
-            print(count,end="")
-            print(',',end="")
-            count = 0
+        isal = str_1[i].isalpha() | str_1[i].isspace()
+        if(isal == 1 ):
+            if(str_1[i] == ' '):
+                print(count,end="")
+                print(',',end="")
+                count = 0
+            else:
+                count+=1
         else:
-            count+=1
+            count = count + 0
+    print(count)

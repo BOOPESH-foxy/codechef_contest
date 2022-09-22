4b60bc8487202bd3d4edbec2e4feb551ee0a0bfe
BOOPESH-foxy <boopesh.mc@gmail.com>
2022-09-22 14:17:06 +0530

    eyrc_codechef find topper

--- src/scripts/dec_bin.py
+++ src/scripts/dec_bin.py
@@ -1,20 +1,14 @@
+import numpy as arr
+
+def dec_to_binary(n):
+    if n>1:
+        dec_to_binary(n//2)
+    print(n%2,end = "")
+
 t = int(input())
-if(t<=25 and t>=1):
+if(t>=1 and t<=25):
     for _ in range(t):
-        n = int(input())
-        if (n>=2 and n<=100):
-            empty_lst = []
-            score_list = []
-            for _ in range(n):
-                name , score = input().split()
-                float(score)
-                if(float(score)>=0.0 and float(score)<=100.0):
-                    score_list.append([score,name])
-            score_list.sort(reverse=True)
-            score_top = score_list[0][0] 
-        for _ in range(len(score_list)):
-            if(score_top == score_list[_][0]):
-                empty_lst.append(score_list[_][1])
-        empty_lst.sort()
-        for _ in range(len(empty_lst)):
-            print(empty_lst[_])
+        dec_data = int(input())
+        if(dec_data>=1 and dec_data<=255):
+            dec_to_binary(dec_data)
+        print()
--- src/scripts/find_topper.py
+++ src/scripts/find_topper.py
@@ -1,20 +1,20 @@
 t = int(input())
-if
-for _ in range(t):
-    n = int(input())
-    if (n>=2 and n<=100):
-        empty_lst = []
-        score_list = []
-        for _ in range(n):
-            name , score = input().split()
-            float(score)
-            if(float(score)>=0.0 and float(score)<=100.0):
-                score_list.append([score,name])
-        score_list.sort(reverse=True)
-        score_top = score_list[0][0] 
-for _ in range(len(score_list)):
-    if(score_top == score_list[_][0]):
-        empty_lst.append(score_list[_][1])
-empty_lst.sort()
-for _ in range(len(empty_lst)):
-    print(empty_lst[_])
+if(t<=25 and t>=1):
+    for _ in range(t):
+        n = int(input())
+        if (n>=2 and n<=100):
+            empty_lst = []
+            score_list = []
+            for _ in range(n):
+                name , score = input().split()
+                float(score)
+                if(float(score)>=0.0 and float(score)<=100.0):
+                    score_list.append([score,name])
+            score_list.sort(reverse=True)
+            score_top = score_list[0][0] 
+        for _ in range(len(score_list)):
+            if(score_top == score_list[_][0]):
+                empty_lst.append(score_list[_][1])
+        empty_lst.sort()
+        for _ in range(len(empty_lst)):
+            print(empty_lst[_])

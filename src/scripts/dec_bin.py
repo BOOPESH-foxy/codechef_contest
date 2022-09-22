c5d912b3e53c527515b6801f60d88ed5d7ca23c1
BOOPESH-foxy <boopesh.mc@gmail.com>
2022-09-22 15:49:23 +0530

    eyrc_codechef dec_bin

--- src/scripts/dec_bin.py
+++ src/scripts/dec_bin.py
@@ -1,14 +1,25 @@
 import numpy as arr
-
 def dec_to_binary(n):
     if n>1:
         dec_to_binary(n//2)
-    print(n%2,end = "")
-
+    empty_list.append(n%2)
+    return empty_list
+    
 t = int(input())
 if(t>=1 and t<=25):
+    bin_lst = []
     for _ in range(t):
+        empty_list = []
         dec_data = int(input())
-        if(dec_data>=1 and dec_data<=255):
-            dec_to_binary(dec_data)
+        if dec_data == 0:
+            for _ in range(0,8):
+                print("0",end="")
+        elif(dec_data>=1 and dec_data<=255):
+            bin_lst = dec_to_binary(dec_data)
+            d_0 = 8 - len(bin_lst)
+            for _ in range(d_0):
+                print("0",end = "")
+            for _ in range(len(bin_lst)):
+                print(bin_lst[_],end = "")
+
         print()
